efa6bc82aaa762e3742899d1a48ffcc1af17db17
dahomeykid <dahomeykid@gmail.com>
2025-03-26 23:07:55 +0100

    added sample env

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 
+import './App.css'
+
+
 function App() {
     return (
         <Router>
