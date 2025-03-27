b3fd4e7bc5fb209985870f768d221d9af507a65f
dahomeykid <dahomeykid@gmail.com>
2025-03-27 09:46:40 +0100

    added edit page route in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import PostDetails from "./components/PostDetails";
+import EditPost from "./components/EditPost";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                         <PostList />
                     </>} />
                     <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/posts/:id/edit" element={<EditPost />} />
                 </Routes>
             </div>
         </Router>
